97d5f8c5a8fb168fcaf72be4966b557442c89371
Denver <denver@MacBook-Pro-van-Denver.local>
2025-01-14 11:41:44 +0100

    switch to paddleocr for better text recognition

--- video_license_plate_tool/ocr/ocr_utils.py
+++ video_license_plate_tool/ocr/ocr_utils.py
@@ -1,14 +1,12 @@
-# ocr/ocr_utils.py
-
-import pytesseract
 import cv2
 import numpy as np
 import re
-import easyocr
+from paddleocr import PaddleOCR
 
-reader = easyocr.Reader(['en'], gpu=True)  # Enable GPU support
+# Initialize PaddleOCR with English language
+ocr = PaddleOCR(use_angle_cls=True, use_gpu=True, lang='en')
 
-def extract_text_from_image(image):
+def extract_text_from_image(frame):
     """
     Run OCR on the given image (assumes it's already cropped to a license plate).
     :param image: np.ndarray (BGR or grayscale)
@@ -20,15 +18,23 @@ def extract_text_from_image(image):
     # else:
     #     gray = image
 
-    # Use EasyOCR:
-    results = reader.readtext(image)
-    plate_text = " ".join([res[1] for res in results])
+    # Use PaddleOCR
+    results = ocr.ocr(frame, det=False, rec=True, cls=False)
 
-    # Optional: apply threshold
-    # _, thresh = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
-    # For simplicity, let's just feed gray
-    # plate_text = pytesseract.image_to_string(gray, config="--psm 7")
+    plate_text = ""
+    for r in results:
+        scores = r[0][1]
+        if np.isnan(scores):
+            scores = 0
+        else:
+            scores = int(scores * 100)
 
-    # Clean up result: remove non-alphanumeric characters and extra spaces
-    plate_text = re.sub(r"[^a-zA-Z0-9]", "", plate_text)
-    return plate_text.strip()
+        if scores > 60:
+            plate_text = r[0][0]
+
+    pattern = re.compile('[\W_]')  
+    plate_text = pattern.sub('', plate_text)
+    plate_text = plate_text.replace("???", "")
+    plate_text = plate_text.replace("O", "0")
+
+    return str(plate_text)
